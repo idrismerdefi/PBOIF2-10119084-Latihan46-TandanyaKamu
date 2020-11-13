b8e0362c22c253f316fb8e5db5d8e1a540e5deef
idrismerdefi <67642938+idrismerdefi@users.noreply.github.com>
2020-11-13 10:56:44 +0700

    Update PBOIF210119084Latihan46TandanyaKamu.java

--- src/pboif2/pkg10119084/latihan46/tandanyakamu/PBOIF210119084Latihan46TandanyaKamu.java
+++ src/pboif2/pkg10119084/latihan46/tandanyakamu/PBOIF210119084Latihan46TandanyaKamu.java
@@ -6,10 +6,11 @@
 package pboif2.pkg10119084.latihan46.tandanyakamu;
 
 /**
- *
- * @author MERDEFI
+Nama : Muhammad Idris Merdefi
+Kelas : PBOIF2
+NIM : 10119084
+Deskripsi Program : Program ini berisi program untuk menampilkan umur
  */
-
 import java.util.Calendar;
 import java.util.Scanner;
 public class PBOIF210119084Latihan46TandanyaKamu {
