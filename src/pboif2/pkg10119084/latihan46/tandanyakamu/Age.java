5a6bb9ff267228639752ee95b6b3579fc60f1a4e
idrismerdefi <67642938+idrismerdefi@users.noreply.github.com>
2020-11-13 10:56:19 +0700

    Update Age.java

--- src/pboif2/pkg10119084/latihan46/tandanyakamu/Age.java
+++ src/pboif2/pkg10119084/latihan46/tandanyakamu/Age.java
@@ -4,10 +4,11 @@
  * and open the template in the editor.
  */
 package pboif2.pkg10119084.latihan46.tandanyakamu;
-
 /**
- *
- * @author MERDEFI
+Nama : Muhammad Idris Merdefi
+Kelas : PBOIF2
+NIM : 10119084
+Deskripsi Program : Program ini berisi program untuk menampilkan umur
  */
 public class Age {
     private int yearBirth, yearNow;
